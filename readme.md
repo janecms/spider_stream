c1720aa3b9c90f45b2bd9267be89536fc224a513
hellojd <randy_shandong@163.com>
2018-11-04 15:37:49 +0800

    初始化2

--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
-## TODO 
+## 来源
+git@github.com:gsh199449/spider.git
+
+## TODO List
 - 优化模板页面
 - 存储爬虫轨迹
 - 升级ELK Client
 - 升级 webmagic
+
+##  Roadmap
+
+- 接入spring boot stream 
---
+++ src/main/java/com/gs/spider/SpiderStreamApplication.java
@@ -0,0 +1,12 @@
+package com.gs.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderStreamApplication.class, args);
+	}
+}
